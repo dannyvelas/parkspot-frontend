722e6acb05f37f82933e3ea990147bc983cb49f8
Daniel Velasquez <danivelas4@gmail.com>
2022-05-17 21:45:38 -0400

    src/routes/car: moved loaded car into its own component. loaded car comp doesn't need to keep checking whether car is Ok or Err

--- src/routes/car/[id].svelte
+++ src/routes/car/[id].svelte
@@ -19,45 +19,10 @@
 
 <script lang="ts">
 	import type { Result } from '$lib/functional';
-	import { put } from '$lib/api';
-	import { isOk, newOk } from '$lib/functional';
-
-	type EditCarArgs = {
-		color: string;
-		make: string;
-		model: string;
-	};
+	import { isOk } from '$lib/functional';
+	import LoadedCar from './_LoadedCar.svelte';
 
 	export let result: Result<Car>;
-	let carId = '';
-	let editCarArgs: EditCarArgs;
-	let bannerError = '';
-
-	if (isOk(result)) {
-		carId = result.data.id;
-
-		editCarArgs = {
-			color: result.data.color,
-			make: result.data.make,
-			model: result.data.model
-		};
-	}
-
-	const submit = async () => {
-		const payload: Partial<EditCarArgs> = {
-			color: editCarArgs.color || undefined,
-			make: editCarArgs.make || undefined,
-			model: editCarArgs.model || undefined
-		};
-
-		const putRes = await put<Partial<EditCarArgs>, Car>(`api/car/${carId}`, payload, carDecoder);
-		if (!isOk(putRes)) {
-			bannerError = putRes.error;
-			return;
-		}
-
-		result = newOk(putRes.data);
-	};
 </script>
 
 <svelte:head>
@@ -67,20 +32,5 @@
 {#if !isOk(result)}
 	<p>Error: {result.error}</p>
 {:else}
-	<h1>Edit Car</h1>
-	{#if bannerError != ''}
-		<div>
-			<p>Error editing permit: {bannerError}. Please try again later.</p>
-		</div>
-	{/if}
-	<p>License Plate: {result.data.licensePlate}</p>
-	<form on:submit|preventDefault={submit}>
-		Color:
-		<input type="text" placeholder={result.data.color} bind:value={editCarArgs.color} />
-		Make:
-		<input type="text" placeholder={result.data.make} bind:value={editCarArgs.make} />
-		Model:
-		<input type="text" placeholder={result.data.model} bind:value={editCarArgs.model} />
-		<button type="submit">Submit</button>
-	</form>
+	<LoadedCar car={result.data} />
 {/if}
