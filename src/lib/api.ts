c80e76267b3b9b2bc71f83b25f8a5ab036770fc2
Daniel Velasquez <danivelas4@gmail.com>
2022-05-17 21:24:14 -0400

    lib/api: handled more errors, turning exceptions to Results

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,52 @@
 import type { Result } from '$lib/functional';
-import { newOk, newErr } from '$lib/functional';
+import { isOk, newOk, newErr } from '$lib/functional';
 import type { Decoder } from 'decoders';
 type Method = 'GET' | 'DELETE' | 'POST' | 'PUT';
+
+const HOSTNAME = import.meta.env.VITE_DEV_SERVER;
 
 type FetchOpts = {
 	method: Method;
 	headers?: Headers;
 	body?: string;
 	credentials: 'include';
+};
+
+const getResponse = async (path: string, fetchOpts: FetchOpts): Promise<Result<Response>> => {
+	try {
+		const response = await fetch(`${HOSTNAME}/${path}`, fetchOpts);
+		return newOk(response);
+	} catch (error) {
+		if (error instanceof TypeError) {
+			return newErr(error.message);
+		} else {
+			return newErr('Unhandled error connecting to server');
+		}
+	}
+};
+
+const getJson = async (response: Response): Promise<Result<any>> => {
+	try {
+		const jsonResponse = await response.json();
+		return newOk(jsonResponse);
+	} catch (error) {
+		if (error instanceof SyntaxError) {
+			return newErr(error.message);
+		} else {
+			return newErr('Unhandled error parsing JSON');
+		}
+	}
+};
+
+const decodeJson = <T>(json: any, responseDecoder: Decoder<T>): Result<T> => {
+	const decodedResponse = responseDecoder.decode(json);
+	if (decodedResponse.ok) {
+		return newOk(decodedResponse.value);
+	} else if (decodedResponse.error.text !== undefined) {
+		return newErr(decodedResponse.error.text);
+	} else {
+		return newErr('Unhandled error decoding response');
+	}
 };
 
 const send =
@@ -17,36 +56,38 @@ const send =
 		data: ReqBody,
 		responseDecoder: Decoder<ResBody>
 	): Promise<Result<ResBody>> => {
-		const opts: FetchOpts = { method, credentials: 'include' };
+		const fetchOpts: FetchOpts = { method, credentials: 'include' };
 		if (data) {
-			opts.headers = new Headers({ 'Content-Type': 'application/json' });
-			opts.body = JSON.stringify(data);
+			fetchOpts.headers = new Headers({ 'Content-Type': 'application/json' });
+			fetchOpts.body = JSON.stringify(data);
 		}
 
-		try {
-			const response = await fetch(`${import.meta.env.VITE_DEV_SERVER}/${path}`, opts);
-			if (response.ok) {
-				try {
-					const jsonResponse = await response.json();
-					const decodedResponse = responseDecoder.verify(jsonResponse);
-					return newOk(decodedResponse);
-				} catch (error) {
-					return newErr(`Error parsing json: ${error}`);
-				}
-			} else {
-				const statusText = `${response.status}: ${response.statusText}`;
-				let errorText = statusText;
+		const response = await getResponse(path, fetchOpts);
+		if (!isOk(response)) {
+			return newErr(response.error);
+		} else if (!response.data.ok) {
+			const statusText = `${response.data.status}: ${response.data.statusText}`;
+			let errorText = statusText;
 
-				const responseText = await response.text();
-				if (responseText) {
-					errorText = `${errorText}. ${responseText}`;
-				}
-
-				return newErr(errorText);
+			const responseText = await response.data.text();
+			if (responseText) {
+				errorText = `${errorText}. ${responseText}`;
 			}
-		} catch (error) {
-			return newErr(`Error sending request: ${error}`);
+
+			return newErr(errorText);
 		}
+
+		const jsonResponse = await getJson(response.data);
+		if (!isOk(jsonResponse)) {
+			return newErr(jsonResponse.error);
+		}
+
+		const decodedResponse = decodeJson(jsonResponse.data, responseDecoder);
+		if (!isOk(decodedResponse)) {
+			return newErr(decodedResponse.error);
+		}
+
+		return newOk(decodedResponse.data);
 	};
 
 export const get = <ResBody>(path: string, responseDecoder: Decoder<ResBody>) =>
