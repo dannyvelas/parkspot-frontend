35cde31c6ccecbb938c73b9cf40dceac5ac610f4
Daniel Velasquez <danivelas4@gmail.com>
2022-05-23 22:53:20 -0400

    src/lib/loadFn.ts: added params and currPageNum to loadfn input and output

--- src/lib/loadFn.ts
+++ src/lib/loadFn.ts
@@ -7,17 +7,26 @@ type OutputProps<T> = {
 	result: T;
 };
 
-export const getLoadFn = <T>(route: string, resourceDecoder: Decoder<T>) => {
+export const getLoadFn = <T>(
+	path: string,
+	params: Record<string, string>,
+	resourceDecoder: Decoder<T>
+) => {
 	const loadFn: Load<Record<string, string>, Record<string, any>, OutputProps<Result<T>>> = async ({
-		session: { user }
+		session: { user },
+		url
 	}) => {
 		if (!user) return { status: 302, redirect: '/' };
 
-		const result = await get<T>(route, resourceDecoder);
+		const currPageNum = Number(url.searchParams.get('page')) || 1;
+		params['page'] = `${currPageNum}`;
+
+		const result = await get<T>(path, params, resourceDecoder);
 
 		return {
 			props: {
-				result
+				result,
+				currPageNum
 			}
 		};
 	};
