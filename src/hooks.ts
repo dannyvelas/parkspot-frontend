7c3b66664563092f48ea84a7e2fbd5038233d454
Daniel Velasquez <danivelas4@gmail.com>
2022-05-24 17:07:51 -0400

    hooks: made logic for verifyingJWT more abstracted and catching expiration exception

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,10 @@
 import * as cookie from 'cookie';
 import jwt from 'jsonwebtoken';
 import { userDecoder } from '$lib/models';
+import { isOk, newErr, newOk } from '$lib/functional';
+import { TokenExpiredError } from 'jsonwebtoken';
+import type { JwtPayload } from 'jsonwebtoken';
+import type { Result } from '$lib/functional';
 import type { Handle, GetSession } from '@sveltejs/kit';
 
 const TOKEN_SECRET = import.meta.env.VITE_TOKEN_SECRET;
@@ -11,14 +15,12 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return await resolve(event);
 	}
 
-	const verifiedJWT = jwt.verify(cookies.jwt, TOKEN_SECRET);
-	if (typeof verifiedJWT === 'string') {
-		return await resolve(event);
-	} else if (!verifiedJWT.user) {
+	const verifiedJWT = verifyJWT(cookies.jwt);
+	if (!isOk(verifiedJWT)) {
 		return await resolve(event);
 	}
 
-	const decodedJWT = userDecoder.decode(verifiedJWT.user);
+	const decodedJWT = userDecoder.decode(verifiedJWT.data.user);
 	if (!decodedJWT.ok) {
 		return await resolve(event);
 	}
@@ -32,4 +34,25 @@ export const getSession: GetSession = (event) => {
 	return {
 		user: event.locals && event.locals.user
 	};
+};
+
+const verifyJWT = (token: string): Result<{ user: string }> => {
+	let verifiedJWT: string | JwtPayload;
+	try {
+		verifiedJWT = jwt.verify(token, TOKEN_SECRET);
+	} catch (error) {
+		if (error instanceof TokenExpiredError) {
+			return newErr('JWT expired');
+		} else {
+			return newErr('Unhandled JWT verify error');
+		}
+	}
+
+	if (typeof verifiedJWT === 'string') {
+		return newErr('JWT was string');
+	} else if (!('user' in verifiedJWT)) {
+		return newErr("JWT did not have 'user' field");
+	}
+
+	return newOk({ user: verifiedJWT.user });
 };
