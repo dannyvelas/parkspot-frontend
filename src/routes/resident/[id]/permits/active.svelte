788e8fb6c1c03606f4260f5078d2c23d3136d64c
Daniel Velasquez <danivelas4@gmail.com>
2022-06-05 16:40:03 -0400

    src/routes/resident/[id]/permits/: added active.svelte and index.svelte (these are pages to see a residents active and all permits)

---
+++ src/routes/resident/[id]/permits/active.svelte
@@ -0,0 +1,54 @@
+<script context="module" lang="ts">
+	import { DEFAULT_AMT_PER_PAGE } from '$lib/constants';
+	import { getLoadFn } from '$lib/loadPermits';
+	import type { Load } from '@sveltejs/kit';
+
+	const limit = DEFAULT_AMT_PER_PAGE;
+
+	export const load: Load = async (args) => {
+		const residentId = args.params.id;
+		const out = await getLoadFn(`api/resident/${residentId}/permits/active`, limit, false)(args);
+		return {
+			props: {
+				residentId,
+				...out.props
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	import type { Result } from '$lib/functional';
+	import type { Permit, ListWithMetadata } from '$lib/models';
+	import { isOk } from '$lib/functional';
+	import PermitsList from '$lib/_PermitsList.svelte';
+
+	export let result: Result<ListWithMetadata<Permit>>;
+	export let currPageNum: number;
+	export let residentId: string;
+</script>
+
+<svelte:head>
+	<title>Active Permits</title>
+</svelte:head>
+
+<h1>Active Permits</h1>
+
+{#if !isOk(result)}
+	{result.message}
+{:else}
+	<PermitsList
+		listType="active"
+		permits={result.data.records}
+		totalAmount={result.data.metadata.totalAmount}
+		pageToHref={(pageNum) => `/resident/${residentId}/permits/active?page=${pageNum}`}
+		{currPageNum}
+		{limit}
+	/>
+{/if}
+
+<style>
+	h1 {
+		text-align: center;
+	}
+</style>
