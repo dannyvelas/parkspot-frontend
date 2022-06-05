433334a491348d4b76dcc8933e8928e8522ec1ce
Daniel Velasquez <danivelas4@gmail.com>
2022-06-05 16:39:51 -0400

    routes/resident/index.svelte: added

---
+++ src/routes/resident/index.svelte
@@ -0,0 +1,49 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ session }) => {
+		if (!session.user) return { status: 302, redirect: '/' };
+
+		return {
+			props: {
+				user: session.user
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	import type { User } from '$lib/models';
+	export let user: User;
+</script>
+
+<h1>Resident Page</h1>
+
+<main>
+	<p>
+		List Active Parking Permits
+		<a href="/resident/{user.id}/permits/active"><button>Submit</button></a>
+	</p>
+	<p>
+		List All Parking Permits:
+		<a href="/resident/{user.id}/permits"><button>Submit</button></a>
+	</p>
+
+	<hr />
+
+	<p>
+		Request a Guest Parking Permit:
+		<a href="/permit/request"><button>Submit</button></a>
+	</p>
+	<p>
+		Add a visitor:
+		<a href="/resident/{user.id}/visitors"><button>Submit</button></a>
+	</p>
+</main>
+
+<style>
+	h1,
+	main {
+		text-align: center;
+	}
+</style>
