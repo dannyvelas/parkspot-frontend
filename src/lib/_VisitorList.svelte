3f06564fa9ba6d055a5b676eb4d560720a60d93d
Daniel Velasquez <danivelas4@gmail.com>
2022-07-03 21:02:20 -0400

    src/lib/_VisitorList.svelte: added

---
+++ src/lib/_VisitorList.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import type { Visitor } from '$lib/models';
+	import Pagination from '$lib/Pagination.svelte';
+
+	// props
+	export let visitors: Array<Visitor>;
+	export let totalAmount: number;
+	export let pageToHref: (a: number) => string; // pagination
+	export let currPageNum: number; // pagination
+	export let limit: number; // pagination
+
+	// rendering
+	const renderDate = (date: Date): string => {
+		const dateStr = date.toISOString();
+		return dateStr.split('T')[0];
+	};
+</script>
+
+<svelte:head>
+	<title>All Visitors</title>
+</svelte:head>
+
+<div>
+	<h2>Amount of Visitors: {totalAmount}</h2>
+	<div>
+		<table>
+			<tr>
+				<td>Resident ID</td>
+				<td>First Name</td>
+				<td>Last Name</td>
+				<td>Relationship</td>
+				<td>Access Start</td>
+				<td>Access End</td>
+			</tr>
+			{#each visitors as visitor}
+				<tr>
+					<td>{visitor.residentId}</td>
+					<td>{visitor.firstName}</td>
+					<td>{visitor.lastName}</td>
+					<td>{visitor.relationship}</td>
+					<td>{renderDate(visitor.accessStart)}</td>
+					<td>{renderDate(visitor.accessEnd)}</td>
+				</tr>
+			{/each}
+		</table>
+	</div>
+	<Pagination {totalAmount} {pageToHref} {currPageNum} {limit} />
+</div>
+
+<style>
+	table,
+	td {
+		border: 1px solid black;
+	}
+
+	h2 {
+		text-align: center;
+	}
+</style>
