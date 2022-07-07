3e2689676eda63b35f7a4a07c75037fe60af4732
Daniel Velasquez <danivelas4@gmail.com>
2022-07-07 12:15:02 -0400

    src/routes/forgot-password.svelte and src/routes/reset-password.svelte: added

---
+++ src/routes/forgot-password.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { post } from "$lib/api";
+  import { messageDecoder } from "$lib/models";
+  import { isOk } from "$lib/functional";
+
+  let id = "";
+  let banner = "";
+
+  const submit = async () => {
+    const payload = { id };
+
+    const postRes = await post(`api/password-reset-email`, payload, messageDecoder);
+    if (!isOk(postRes)) {
+      banner = postRes.message;
+      return;
+    }
+
+    banner = postRes.data.message;
+  };
+</script>
+
+<h1>Request a Password Reset Email</h1>
+{#if banner != ""}
+  <div>
+    <p>{banner}</p>
+  </div>
+{/if}
+<form on:submit|preventDefault={submit}>
+  <input required type="text" placeholder="T8100123" bind:value={id} />
+  <button type="submit" style="margin:10px;">Send Email To Reset Password</button>
+</form>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+
+  form {
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  input[type="text"] {
+    padding: 12px 20px;
+    margin: 8px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+</style>
---
+++ src/routes/reset-password.svelte
@@ -0,0 +1,92 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = (loadInput) => {
+    const accessToken = loadInput.url.searchParams.get("token");
+    if (accessToken === null) {
+      return { status: 302, redirect: "/" };
+    }
+
+    return {
+      props: {
+        accessToken,
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  import { put } from "$lib/api";
+  import { messageDecoder } from "$lib/models";
+  import { isOk } from "$lib/functional";
+
+  // props
+  export let accessToken: string;
+
+  // model
+  let password = "";
+  let confirmPassword = "";
+  let passwordsShown = false;
+  $: passwordType = passwordsShown ? "text" : "password";
+  let banner = "";
+
+  // events
+  const inputPassword = (e: any) => {
+    password = e.target.value;
+  };
+  const inputConfirmPassword = (e: any) => {
+    confirmPassword = e.target.value;
+  };
+
+  const submit = async () => {
+    if (password !== confirmPassword) {
+      banner = "Error: passwords do not match";
+      return;
+    }
+
+    const payload = { password };
+    const putRes = await put(`api/account/password`, payload, messageDecoder, accessToken);
+    if (!isOk(putRes)) {
+      banner = putRes.message;
+      return;
+    }
+
+    banner = putRes.data.message;
+  };
+</script>
+
+<h1>Password Reset</h1>
+{#if banner != ""}
+  <div>
+    <p>{banner}</p>
+  </div>
+{/if}
+<form on:submit|preventDefault={submit}>
+  <input type={passwordType} placeholder="Password" on:input={inputPassword} />
+  <input type={passwordType} placeholder="Confirm your Password" on:input={inputConfirmPassword} />
+  <div style="margin:20px;">
+    <label for="showPasswords">Show Passwords: </label>
+    <input type="checkbox" id="showPasswords" bind:checked={passwordsShown} />
+  </div>
+  <button type="submit">Reset Password</button>
+</form>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+
+  form {
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  input {
+    padding: 12px 20px;
+    margin: 8px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+</style>
