9cc4fd50835a70019b5951eda956ed4f6891f3fa
Daniel Velasquez <danivelas4@gmail.com>
2022-07-11 23:04:00 -0400

    src/routes/login.svelte: added styling, show password button, and removed unnecessary goto

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -15,7 +15,6 @@
   import { userDecoder } from "$lib/models";
   import { isOk } from "$lib/functional";
   import { getStores } from "$app/stores";
-  import { goto } from "$app/navigation";
   import { post } from "$lib/api";
   const { session } = getStores();
 
@@ -23,8 +22,14 @@
   let id = "";
   let password = "";
   let error = "";
+  let passwordsShown = false;
+  $: passwordType = passwordsShown ? "text" : "password";
 
   // events
+  const inputPassword = (e: any) => {
+    password = e.target.value;
+  };
+
   const submit = async () => {
     const result = await post("api/login", { id, password }, userDecoder);
     if (!isOk(result)) {
@@ -33,7 +38,6 @@
     }
 
     $session.user = result.data;
-    goto(dashboard($session.user));
   };
 </script>
 
@@ -44,14 +48,30 @@
   <p>{error}</p>
 {/if}
 <form on:submit|preventDefault={submit}>
-  <fieldset class="form-group">
-    <input required type="username" placeholder="Username" bind:value={id} />
-  </fieldset>
-  <fieldset class="form-group">
-    <input required type="password" placeholder="Password" bind:value={password} />
-  </fieldset>
+  <input required type="username" placeholder="Username" bind:value={id} />
+  <input required type={passwordType} placeholder="Password" on:input={inputPassword} />
+  <div style="margin:20px;">
+    <label for="showPasswords">Show Passwords: </label>
+    <input type="checkbox" id="showPasswords" bind:checked={passwordsShown} />
+  </div>
   <button type="submit"> Sign in </button>
 </form>
-<div>
+<div style="text-align: center; margin:20px">
   <a href="/forgot-password">Forgot your Password?</a>
 </div>
+
+<style>
+  form {
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  input {
+    padding: 12px 20px;
+    margin: 8px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+</style>
