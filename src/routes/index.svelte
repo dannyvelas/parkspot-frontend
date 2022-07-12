611e2a2988bfa34c79e349af62200f7f0154f913
Daniel Velasquez <danivelas4@gmail.com>
2022-07-12 14:50:53 -0400

    index: styled similar to original lasvistas page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,74 @@
-<h1>index</h1>
+<header>
+  <h1>Las Vistas Parking and Visitor List</h1>
+</header>
+<article>
+  <section>
+    <h2>Temporary Parking Permit Directions</h2>
+    <ol>
+      <li>
+        <b>There exists only one account per apartment</b>
+      </li>
+      <li>
+        <b>Usernames:</b>
+        <ul>
+          <li>A resident owner's username starts with "B". Eg: B8100123</li>
+          <li>A tenant username starts with "T". Eg: T8100123</li>
+        </ul>
+      </li>
+      <li>
+        <b>Only Las Vistas residents can request parking permits</b>
+      </li>
+    </ol>
+  </section>
+  <section>
+    <h2>Parking Regulations</h2>
+    <ol>
+      <li>
+        <b>A permit request can last from 1 day to 15 days.</b> If a resident wants their guest to park
+        for longer than 15 days, they must submit another request after their first 15 day request expires.
+      </li>
+      <li>
+        <b>Each resident can give out max 20 days worth of parking permits per year.</b>
+        In order to park for 20 days continously, a resident must apply for two consecutive permits of
+        15 days.
+      </li>
+      <li>
+        <b>Each guest car can park max 20 days with temporary parking permits per year.</b>
+        If a guest car has already parked 30 days using parking permits, this car does not qualify for
+        further parking.
+      </li>
+      <li>
+        <b>A resident can have max 2 active permits at a given time.</b>
+        If a resident requests a permit to start at a future date, this is considered an "active" request
+        even if the future date has not yet arrived. A permit request stops being active once the expiration
+        date of the permit arrives.
+      </li>
+    </ol>
+  </section>
+</article>
+
+<style>
+  header {
+    background-image: url("./static/miami.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    display: flex;
+    height: 500px;
+    position: relative;
+  }
+
+  article {
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    width: 80%;
+  }
+
+  h1 {
+    margin: 10px;
+    color: #ccccff;
+    text-shadow: 0.5px 0.5px black;
+    font-size: 2em;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
-import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-node";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter(),
+    vite: {
+      server: {
+        fs: {
+          allow: ["./static"],
+        },
+      },
+    },
+  },
 };
 
 export default config;
