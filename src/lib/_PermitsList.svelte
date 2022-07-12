77f3366f1ecf254b3c4173043fbb72e37d602c0f
Daniel Velasquez <danivelas4@gmail.com>
2022-07-12 13:23:17 -0400

    src/lib/_PermitsList.svelte: removed unnecessary divs

--- src/lib/_PermitsList.svelte
+++ src/lib/_PermitsList.svelte
@@ -43,50 +43,46 @@
   };
 </script>
 
-<div>
-  <h2>Amount of Permits: {totalAmount}</h2>
-  <div>
-    <table>
-      <tr>
-        <td>Permit ID</td>
-        <td>Resident ID</td>
-        <td>License Plate</td>
-        <td>Color</td>
-        <td>Make</td>
-        <td>Model</td>
-        <td>Start Date</td>
-        <td>End Date</td>
-        <td>Request Date</td>
-        {#if listType === "exceptions"}
-          <td>Exception Reason</td>
-        {/if}
-        <td>Reprint</td>
-        <td>Edit</td>
-        <td>Delete</td>
-      </tr>
-      {#each permits as permit, i (permit.id)}
-        <tr>
-          <td>{permit.id}</td>
-          <td>{permit.residentId}</td>
-          <td>{permit.car.licensePlate}</td>
-          <td>{permit.car.color}</td>
-          <td>{permit.car.make}</td>
-          <td>{permit.car.model}</td>
-          <td>{renderDate(permit.startDate)}</td>
-          <td>{renderDate(permit.endDate)}</td>
-          <td>{tsToDate(permit.requestTS)}</td>
-          {#if listType === "exceptions"}
-            <td>{permit.exceptionReason}</td>
-          {/if}
-          <td><a href="/permit/{permit.id}">Reprint</a></td>
-          <td><a href="car/{permit.car.id}">Edit</a></td>
-          <td><button on:click={() => deletePermit(i, permit.id)}>Delete</button></td>
-        </tr>
-      {/each}
-    </table>
-  </div>
-  <Pagination {totalAmount} {pageToHref} {currPageNum} {limit} />
-</div>
+<h2>Amount of Permits: {totalAmount}</h2>
+<table>
+  <tr>
+    <td>Permit ID</td>
+    <td>Resident ID</td>
+    <td>License Plate</td>
+    <td>Color</td>
+    <td>Make</td>
+    <td>Model</td>
+    <td>Start Date</td>
+    <td>End Date</td>
+    <td>Request Date</td>
+    {#if listType === "exceptions"}
+      <td>Exception Reason</td>
+    {/if}
+    <td>Reprint</td>
+    <td>Edit</td>
+    <td>Delete</td>
+  </tr>
+  {#each permits as permit, i (permit.id)}
+    <tr>
+      <td>{permit.id}</td>
+      <td>{permit.residentId}</td>
+      <td>{permit.car.licensePlate}</td>
+      <td>{permit.car.color}</td>
+      <td>{permit.car.make}</td>
+      <td>{permit.car.model}</td>
+      <td>{renderDate(permit.startDate)}</td>
+      <td>{renderDate(permit.endDate)}</td>
+      <td>{tsToDate(permit.requestTS)}</td>
+      {#if listType === "exceptions"}
+        <td>{permit.exceptionReason}</td>
+      {/if}
+      <td><a href="/permit/{permit.id}">Reprint</a></td>
+      <td><a href="car/{permit.car.id}">Edit</a></td>
+      <td><button on:click={() => deletePermit(i, permit.id)}>Delete</button></td>
+    </tr>
+  {/each}
+</table>
+<Pagination {totalAmount} {pageToHref} {currPageNum} {limit} />
 
 <style>
   table,
