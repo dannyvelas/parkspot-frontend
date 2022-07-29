b10a8c8ef212350a758622f40ee13bf31b9bcc2b
Daniel Velasquez <danivelas4@gmail.com>
2022-07-29 09:32:20 -0400

    src/routes: added custom error page. it makes sure that /index and /index.php don't return 404s

---
+++ src/routes/__error.svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = (loadInput) => {
+    if (loadInput.url.pathname === "/index" || loadInput.url.pathname === "/index.php") {
+      return { status: 302, redirect: "/" };
+    }
+
+    return {
+      props: {
+        status: loadInput.status || undefined,
+        message: loadInput.error?.message,
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let status: number | undefined;
+  export let message: string | undefined;
+</script>
+
+<h1>{status}</h1>
+<h2>Error: {message}</h2>
