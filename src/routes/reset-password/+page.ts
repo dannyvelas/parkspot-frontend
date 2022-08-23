68180e210b6b33d7399dddbac8993c91c1527e9a
Daniel Velasquez <danivelas4@gmail.com>
2022-08-23 10:42:02 -0400

    src/routes/reset-password/+page.ts: now, passing in accessToken into data

--- src/routes/reset-password/+page.ts
+++ src/routes/reset-password/+page.ts
@@ -2,11 +2,16 @@ import type { PageLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 import { dashboard } from "$lib/navigation";
 
-export const load: PageLoad = async ({ parent }) => {
-  const parentData = await parent();
+export const load: PageLoad = async (loadInput) => {
+  const parentData = await loadInput.parent();
+  const accessToken = loadInput.url.searchParams.get("token");
   if (parentData.user) {
     throw redirect(307, dashboard(parentData.user));
+  } else if (accessToken == "" || accessToken == null) {
+    throw redirect(307, "/login");
   }
 
-  return {};
+  return {
+    accessToken,
+  };
 };
