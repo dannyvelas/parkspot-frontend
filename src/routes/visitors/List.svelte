8230856a6a15dfb77853f93efbd34485463090df
Daniel Velasquez <danivelas4@gmail.com>
2022-08-23 10:46:00 -0400

    src/routes/visitors/List.svelte: added, i meant to add this a few commits ago

---
+++ src/routes/visitors/List.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import type { Visitor } from "$lib/models";
+  import { del } from "$lib/api";
+  import { isOk } from "$lib/functional";
+  import { dateToYmd } from "$lib/convert";
+
+  // props
+  export let userRole: string;
+  export let visitors: Array<Visitor>;
+  export let totalAmount: number;
+
+  // events
+  const deleteVisitor = async (i: number, visitorId: string, fullName: string) => {
+    if (confirm(`Are you sure you want to delete ${fullName}?`)) {
+      const delRes = await del(`api/visitor/${visitorId}`);
+      if (!isOk(delRes)) {
+        alert(`Error deleting visitor ${fullName}. Please try again later`);
+        return;
+      }
+
+      visitors = [...visitors.slice(0, i), ...visitors.slice(i + 1)];
+
+      alert(`Deleted visitor ${fullName}`);
+    }
+  };
+</script>
+
+<svelte:head>
+  <title>All Visitors</title>
+</svelte:head>
+
+<div>
+  <h2>Amount of Visitors: {totalAmount}</h2>
+  <div>
+    <table>
+      <tr>
+        <td>Resident ID</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>Relationship</td>
+        <td>Access Start</td>
+        <td>Access End</td>
+        {#if userRole === "resident"}
+          <td>Delete</td>
+        {/if}
+      </tr>
+      {#each visitors as visitor, i (visitor.id)}
+        <tr>
+          <td>{visitor.residentId}</td>
+          <td>{visitor.firstName}</td>
+          <td>{visitor.lastName}</td>
+          <td>{visitor.relationship}</td>
+          <td>{dateToYmd(visitor.accessStart)}</td>
+          <td>{dateToYmd(visitor.accessEnd)}</td>
+          {#if userRole === "resident"}
+            <td
+              ><button
+                on:click={() =>
+                  deleteVisitor(i, visitor.id, visitor.firstName + " " + visitor.lastName)}
+                >Delete</button
+              ></td
+            >
+          {/if}
+        </tr>
+      {/each}
+    </table>
+  </div>
+</div>
+
+<style>
+  table,
+  td {
+    border: 1px solid black;
+  }
+
+  h2 {
+    text-align: center;
+  }
+</style>
