fd3a9c7f3d92935c297dc7afb382ffd98255fbf7
Daniel Velasquez <danivelas4@gmail.com>
2022-09-09 18:31:40 -0400

    src/routes/permit/+page.ts: updated with parentData schema

--- src/routes/permit/+page.ts
+++ src/routes/permit/+page.ts
@@ -3,14 +3,8 @@ import { redirect } from "@sveltejs/kit";
 
 export const load: PageLoad = async ({ parent }) => {
   const parentData = await parent();
-  if (!parentData.user) {
+  if (!parentData.session) {
     throw redirect(307, "/login");
   }
-
-  const residentId = parentData.user.role === "resident" ? parentData.user.id : "";
-
-  return {
-    residentId,
-    user: parentData.user,
-  };
+  return { session: parentData.session };
 };
