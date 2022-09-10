59d39b8e66f1d3f5bfcafa3cd069981ba451de9f
Daniel Velasquez <danivelas4@gmail.com>
2022-09-10 15:07:29 -0400

    src/lib/auth/role.ts: fixed to use session, not user

--- src/lib/auth/role.ts
+++ src/lib/auth/role.ts
@@ -1,13 +1,13 @@
-import type { User } from "$lib/models";
+import type { Session } from "$lib/auth";
 import { dashboard } from "$lib/navigation";
 import { redirect } from "@sveltejs/kit";
 
-export const onlyRole = (role: string, user?: User): User => {
-  if (!user) {
+export const onlyRole = (role: string, session: Session | undefined): Session => {
+  if (!session) {
     throw redirect(307, "/login");
-  } else if (user.role !== role) {
-    throw redirect(307, dashboard(user));
+  } else if (session.user.role !== role) {
+    throw redirect(307, dashboard(session.user));
   }
 
-  return user;
+  return session;
 };
