c2eae19dd5f74e1c879dd6e334062dae05ab2b62
Daniel Velasquez <danivelas4@gmail.com>
2022-09-12 21:04:36 -0400

    src/routes/+layout.server.ts: added hacky way for this to run on navigation

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,12 +4,17 @@ import { isOk } from "$lib/functional";
 import { sessionDecoder } from "$lib/auth";
 import { post } from "$lib/api";
 
-export const load: LayoutServerLoad = async ({ request }) => {
-  const cookies = parse(request.headers.get("cookie") || "");
+export const load: LayoutServerLoad = async (loadInput) => {
+  // HACK: make this run for every single page-load
+  // probably use invalidate once this https://github.com/sveltejs/kit/issues/6459 gets fixed
+  loadInput.url.pathname;
+
+  const cookies = parse(loadInput.request.headers.get("cookie") || "");
   if (!cookies.refresh) {
     return {};
   }
 
+  // TODO: only call this if accessToken is about to expire
   const sessionRes = await post("api/refresh-tokens", {}, sessionDecoder, "", [
     ["cookie", `refresh=${cookies.refresh}`],
   ]);
