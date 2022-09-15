afa0fd2fea6531508eaa570fc35a546ff155ef8b
Daniel Velasquez <danivelas4@gmail.com>
2022-09-14 20:59:28 -0400

    src/routes/+layout.server.ts: added temporary logging statements to see accessToken

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,22 +5,21 @@ import { sessionDecoder } from "$lib/auth";
 import { post } from "$lib/api";
 
 export const load: LayoutServerLoad = async (loadInput) => {
-  // HACK: make this run for every single page-load
-  // probably use invalidate once this https://github.com/sveltejs/kit/issues/6459 gets fixed
-  loadInput.url.pathname;
+  console.log("RUNNING");
 
   const cookies = parse(loadInput.request.headers.get("cookie") || "");
   if (!cookies.refresh) {
     return {};
   }
 
-  // TODO: only call this if accessToken is about to expire
   const sessionRes = await post("api/refresh-tokens", {}, sessionDecoder, "", [
     ["cookie", `refresh=${cookies.refresh}`],
   ]);
   if (!isOk(sessionRes)) {
     return {};
   }
+
+  console.log(sessionRes.data.accessToken);
 
   return {
     session: {
