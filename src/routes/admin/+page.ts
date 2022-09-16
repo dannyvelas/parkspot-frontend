019df31b72ab225a0bc159bc23c1a8cea02ca280
Daniel Velasquez <danivelas4@gmail.com>
2022-09-15 22:08:14 -0400

    src/routes/admin/+page.ts: deleted bc not being used anymore

--- src/routes/admin/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageLoad } from "./$types";
-import { onlyRole } from "$lib/auth";
-
-export const load: PageLoad = async ({ parent }) => {
-  const parentData = await parent();
-  const session = onlyRole("admin", parentData.session);
-
-  return { session };
-};
