cb30b4ae3555d59cd8ffcd70c8bc30af57412d3d
Daniel Velasquez <danivelas4@gmail.com>
2022-09-17 15:47:07 -0400

    src/routes/resident/[id]/+page.ts: added refresh logic if on client-side

--- src/routes/resident/[id]/+page.ts
+++ src/routes/resident/[id]/+page.ts
@@ -2,20 +2,15 @@ import type { PageLoad } from "./$types";
 import { onlyRole } from "$lib/auth";
 import { get } from "$lib/api";
 import { residentDecoder } from "$lib/models";
+import { getLatestToken } from "$lib/auth";
+import { browser } from "$app/environment";
 
 export const load: PageLoad = async (loadInput) => {
   const parentData = await loadInput.parent();
   const session = onlyRole("admin", parentData.session);
 
-  const result = await get(
-    `api/resident/${loadInput.params.id}`,
-    {},
-    residentDecoder,
-    session.accessToken
-  );
+  const accessToken = !browser ? session.accessToken : await getLatestToken();
+  const result = await get(`api/resident/${loadInput.params.id}`, {}, residentDecoder, accessToken);
 
-  return {
-    result,
-    session,
-  };
+  return { result };
 };
