1a0985d912048559cd71b5d86208e747453b2fec
Daniel Velasquez <danivelas4@gmail.com>
2022-09-16 20:59:03 -0400

    src/lib/auth/store.ts: added refresh to token store

--- src/lib/auth/store.ts
+++ src/lib/auth/store.ts
@@ -1,3 +1,22 @@
 import { writable } from "svelte/store";
+import { post } from "$lib/api";
+import { sessionDecoder } from "$lib/auth";
+import { isOk } from "$lib/functional";
 
-export const tokenStore = writable("");
+export const createTokenStore = () => {
+  const { subscribe, set } = writable("");
+
+  return {
+    subscribe,
+    set,
+    refresh: async () => {
+      const sessionRes = await post("api/refresh-tokens", {}, sessionDecoder, "");
+      if (!isOk(sessionRes)) {
+        return set("");
+      }
+      return set(sessionRes.data.accessToken);
+    },
+  };
+};
+
+export const tokenStore = createTokenStore();
