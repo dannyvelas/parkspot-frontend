27c9551d05cef52ceb62a37363cf7e6c1915c2da
Daniel Velasquez <danivelas4@gmail.com>
2022-09-17 20:35:57 -0400

    src/routes/+layout.ts: making sure tokenstore is cleared when no session

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,8 +3,12 @@ import { browser } from "$app/environment";
 import { tokenStore } from "$lib/auth";
 
 export const load: LayoutLoad = async (loadInput) => {
-  if (browser && loadInput.data.session) {
-    tokenStore.set(loadInput.data.session.accessToken);
+  if (browser) {
+    if (loadInput.data.session) {
+      tokenStore.set(loadInput.data.session.accessToken);
+    } else {
+      tokenStore.set("");
+    }
   }
 
   return loadInput.data;
