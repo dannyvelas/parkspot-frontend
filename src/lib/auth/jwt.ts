4ce032ba59d3ea640fe5583880c4803acc9a6143
Daniel Velasquez <danivelas4@gmail.com>
2022-09-22 15:48:29 -0400

    src/lib/auth/jwt.ts: removed verify refresh bc not used anymore

--- src/lib/auth/jwt.ts
+++ src/lib/auth/jwt.ts
@@ -1,25 +1,7 @@
 import type { User } from "$lib/models";
 import * as jose from "jose";
-import { newErr, newOk } from "$lib/functional";
 
 const REFRESH_SECRET: string = import.meta.env.VITE_TOKEN_REFRESHSECRET;
-
-export const verifyRefresh = async (token: string) => {
-  let verifiedJWT: jose.JWTVerifyResult;
-  try {
-    verifiedJWT = await jose.jwtVerify(token, Buffer.from(REFRESH_SECRET));
-  } catch (err) {
-    if (err instanceof jose.errors.JWTExpired) {
-      return newErr("jwt expired");
-    } else if (err instanceof jose.errors.JWSSignatureVerificationFailed) {
-      return newErr("jwt verification failed");
-    } else {
-      return newErr("unhandled error verifying jwt");
-    }
-  }
-
-  return newOk(verifiedJWT);
-};
 
 export const newRefresh = async (user: User) => {
   return await new jose.SignJWT({ user })
