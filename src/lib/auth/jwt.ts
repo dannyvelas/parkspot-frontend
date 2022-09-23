c93743ef8e1f6cb44e8c6d5e18f91ce92495aba8
Daniel Velasquez <danivelas4@gmail.com>
2022-09-23 15:00:50 -0400

    src/lib/auth/jwt.ts: moved if statement

--- src/lib/auth/jwt.ts
+++ src/lib/auth/jwt.ts
@@ -14,14 +14,15 @@ export const newRefresh = async (user: User) => {
 export const expiringSoon = (token: string) => {
   try {
     const payload = jose.decodeJwt(token);
+    if (!payload.exp) {
+      return true;
+    }
 
     const epochTimeNow = (() => {
       var d = new Date();
       return Math.round(d.getTime() / 1000);
     })();
-    if (!payload.exp) {
-      return true;
-    }
+
     const secsToExpiry = payload.exp - epochTimeNow;
     const minsToExpiry = Math.round(secsToExpiry / 60);
     return minsToExpiry <= 5;
