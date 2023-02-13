54a3402f47b816baf69cff6515e33f9e3d90d0e4
Daniel Velasquez <danivelas4@gmail.com>
2023-02-13 14:22:23 -0500

    src/routes/permits/List.svelte: using flex col and gap to space out table rows instead of adding mb-2 to each one

--- src/routes/permits/List.svelte
+++ src/routes/permits/List.svelte
@@ -27,16 +27,18 @@
   };
 </script>
 
-<div
-  class="bg-black rounded text-white mb-2 shadow-md flex flex-row justify-between md:justify-start px-2 md:px-8 py-3"
->
-  <div class="text-xs basis-1/12" />
-  <div class="text-xs hidden md:inline md:basis-1/6">ID</div>
-  <div class="text-xs hidden md:inline md:basis-1/6 ">Resident ID</div>
-  <div class="text-xs basis-1/4 md:basis-2/6">Vehicle</div>
-  <div class="text-xs basis-1/4 md:basis-1/6">License</div>
-  <div class="text-xs basis-1/4 md:basis-1/6 lg:basis-1/12">Status</div>
+<div class="flex flex-col gap-2">
+  <div
+    class="bg-black rounded text-white shadow-md flex flex-row justify-between md:justify-start px-2 md:px-8 py-3"
+  >
+    <div class="text-xs basis-1/12" />
+    <div class="text-xs hidden md:inline md:basis-1/6">ID</div>
+    <div class="text-xs hidden md:inline md:basis-1/6 ">Resident ID</div>
+    <div class="text-xs basis-1/4 md:basis-2/6">Vehicle</div>
+    <div class="text-xs basis-1/4 md:basis-1/6">License</div>
+    <div class="text-xs basis-1/4 md:basis-1/6 lg:basis-1/12">Status</div>
+  </div>
+  {#each permits as permit, i (permit.id)}
+    <Row {permit} />
+  {/each}
 </div>
-{#each permits as permit, i (permit.id)}
-  <Row {permit} />
-{/each}
