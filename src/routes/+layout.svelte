33ecaf61d94297fd793d88a1bdaab0f5b3501257
Daniel Velasquez <danivelas4@gmail.com>
2023-02-26 18:34:51 -0500

    src/routes/+layout.svelte: change from h-full to h-screen made sidebar take up full screen height, not just height of content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,11 +13,15 @@
 
 <div class="relative">
   {#if data.session && sidebarIsOpen}
-    <Sidebar twClasses={"absolute z-20 h-full"} on:closeSidebar={() => (sidebarIsOpen = false)} />
+    <Sidebar twClasses={"absolute z-20 h-screen"} on:closeSidebar={() => (sidebarIsOpen = false)} />
   {/if}
 
   <div
-    class="absolute w-full h-full {sidebarIsOpen ? 'block' : 'hidden'} z-10 bg-gray-800 opacity-25"
+    class="absolute w-full h-screen {sidebarIsOpen
+      ? 'block'
+      : 'hidden'} z-10 bg-gray-800 opacity-25"
+    on:click={() => (sidebarIsOpen = false)}
+    on:keypress={() => (sidebarIsOpen = false)}
   />
 
   <Nav on:openSidebar={() => (sidebarIsOpen = true)} session={data.session} />
