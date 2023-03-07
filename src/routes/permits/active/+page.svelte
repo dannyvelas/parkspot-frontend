ba8b203cd7a08925bfc0616a3ba75837e38daf3f
Daniel Velasquez <danivelas4@gmail.com>
2023-03-07 12:27:59 -0500

    src/routes/permits/active/+page.svelte: updated this page to recent commit changes, which i had forgotten to update

--- src/routes/permits/active/+page.svelte
+++ src/routes/permits/active/+page.svelte
@@ -5,4 +5,9 @@
   export let data: PageData;
 </script>
 
-<Page listName="active" permits={data.permits} session={data.session} />
+<Page
+  listName="active"
+  permits={data.permits}
+  lastSearch={data.lastSearch}
+  session={data.session}
+/>
