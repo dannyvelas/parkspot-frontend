4ebf185241199d7c9110744c9f4208121efa7caf
Daniel Velasquez <danivelas4@gmail.com>
2023-03-09 13:20:06 -0500

    src/lib/components/Pagination.svelte: added lower bound and upper bound working buttons

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -21,19 +21,52 @@
       return int_div;
     }
   }
+
+  function decBounds() {
+    if (leftBound === 1) {
+      return;
+    }
+    if (rightBound - leftBound >= rangeSize) {
+      // if the visible range is smaller than `rangeSize` because we're near the end,
+      // then, if we scroll to the left, by not decrementing the right bound, we can make the
+      // visible range size get closer or equal to `rangeSize`
+      rightBound = rightBound - 1;
+    }
+    leftBound = leftBound - 1;
+  }
+
+  function incBounds() {
+    if (rightBound === amtPages) {
+      return;
+    }
+    leftBound = leftBound + 1;
+    rightBound = rightBound + 1;
+  }
 </script>
 
 {#if pageLinks.length > 1}
   <button on:click={() => console.log(leftBound)}>Hi</button>
-  <nav>
-    <ul class="list-none flex flex-row justify-center">
+  <nav class="flex flex-row justify-center align-center gap-2">
+    <button on:click={decBounds}>
+      <iconify-icon
+        class="text-green-400"
+        icon="material-symbols:arrow-circle-left-outline-rounded"
+      />
+    </button>
+    <ul class="list-none flex flex-row gap-2">
       {#each pageLinks as pageLink}
         {#if pageLink >= leftBound && pageLink <= rightBound}
-          <li class="m-2" class:active={pageNum == pageLink}>
+          <li class:text-green-400={pageNum === pageLink}>
             <a href={pageToHref(pageLink)}>{pageLink}</a>
           </li>
         {/if}
       {/each}
     </ul>
+    <button on:click={incBounds}>
+      <iconify-icon
+        class="text-green-400"
+        icon="material-symbols:arrow-circle-right-outline-rounded"
+      />
+    </button>
   </nav>
 {/if}
