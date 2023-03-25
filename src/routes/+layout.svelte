1722999bb8a71e77994bf88175b881e975c1f8dd
Daniel Velasquez <danivelas4@gmail.com>
2023-03-25 14:43:40 -0400

    src/routes/+layout.svelte: small changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,37 @@
 <script lang="ts">
+  import type { PageData } from "./$types";
   import Nav from "$lib/components/Nav.svelte";
   import Sidebar from "$lib/components/Sidebar.svelte";
   import "../app.css";
-  import type { PageData } from "./$types";
 
   // props
   export let data: PageData;
 
   // model
   let sidebarIsOpen = false;
+
+  // events
+  function closeSidebar() {
+    sidebarIsOpen = false;
+  }
+
+  function openSidebar() {
+    sidebarIsOpen = true;
+  }
 </script>
 
 <div class="relative">
   {#if data.session && sidebarIsOpen}
-    <Sidebar twClasses={"absolute z-20 h-screen"} on:closeSidebar={() => (sidebarIsOpen = false)} />
+    <Sidebar twClasses={"absolute z-20 min-h-full"} on:closeSidebar={closeSidebar} />
   {/if}
 
   <div
-    class="absolute w-full h-screen {sidebarIsOpen
-      ? 'block'
-      : 'hidden'} z-10 bg-gray-800 opacity-25"
-    on:click={() => (sidebarIsOpen = false)}
-    on:keypress={() => (sidebarIsOpen = false)}
+    class="fixed w-full h-screen {sidebarIsOpen ? 'block' : 'hidden'} z-10 bg-gray-800 opacity-25"
+    on:click={closeSidebar}
+    on:keypress={closeSidebar}
   />
 
-  <Nav on:openSidebar={() => (sidebarIsOpen = true)} session={data.session} />
+  <Nav on:openSidebar={openSidebar} session={data.session} />
 
   <main class="px-4 md:px-10">
     <slot />
