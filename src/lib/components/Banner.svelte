b6f9414cd07ca1ea09e75c5f6990b6c7d6e1f999
Daniel Velasquez <danivelas4@gmail.com>
2023-03-30 15:00:45 -0400

    src/lib/components/Banner.svelte: added

---
+++ src/lib/components/Banner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  // model
+  export let banner = { isError: false, msg: "" };
+
+  // styles
+  $: colors = banner.isError ? "bg-rose-200 border-rose-500" : "bg-green-200 border-green-500";
+</script>
+
+{#if banner.msg != ""}
+  <div class="min-w-full {colors} rounded-md border p-2">
+    <p class="text-sm">{banner.msg}</p>
+  </div>
+{/if}
