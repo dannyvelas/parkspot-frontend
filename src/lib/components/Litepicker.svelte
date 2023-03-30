8d7fb4058e80f814dc85c02891f8dd8865f397ab
Daniel Velasquez <danivelas4@gmail.com>
2023-03-30 18:01:22 -0400

    src/lib/components/Litepicker.svelte: added

---
+++ src/lib/components/Litepicker.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { createEventDispatcher } from "svelte";
+
+  // config
+  const dispatch = createEventDispatcher();
+
+  // props
+  export let startDate: Date;
+  export let endDate: Date;
+
+  // init
+  onMount(async () => {
+    const { Litepicker } = await import("litepicker");
+    const litepicker = new Litepicker({
+      element: document.getElementById("litepicker") as HTMLElement,
+      minDays: 2,
+      minDate: startDate,
+      singleMode: false,
+      format: "MM-DD-YYYY",
+    });
+    litepicker.setStartDate(startDate);
+    litepicker.setEndDate(endDate);
+
+    litepicker.on("selected", (date1, date2) => {
+      dispatch("selected", { date1, date2 });
+    });
+  });
+</script>
+
+<input required type="text" name="dates" id="litepicker" />
