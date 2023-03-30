9dc3d20390031803de8aa1b371b77faf0bb90650
Daniel Velasquez <danivelas4@gmail.com>
2023-03-30 15:34:27 -0400

    src/routes/permits/DeletePermit.svelte: added

---
+++ src/routes/permits/DeletePermit.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import type { Permit } from "$lib/models";
+  import { Request } from "$lib/api";
+  import { getLatestToken } from "$lib/auth";
+  import { isOk } from "$lib/functional";
+  import { createEventDispatcher } from "svelte";
+  import Banner from "$lib/components/Banner.svelte";
+
+  // config
+  const dispatch = createEventDispatcher();
+
+  // props
+  export let permit: Permit;
+
+  // model
+  let bannerCfg = { isError: false, msg: "" };
+
+  // events
+  async function handleSubmit() {
+    const delRes = await new Request()
+      .setAccessToken(await getLatestToken())
+      .delete(`api/permit/${permit.id}`);
+    if (!isOk(delRes)) {
+      bannerCfg = { isError: true, msg: delRes.message };
+      return;
+    }
+
+    dispatch("permitDeleted", permit);
+  }
+</script>
+
+<form
+  class="bg-white flex flex-col mx-auto w-52 md:w-64 gap-4"
+  on:submit|preventDefault={handleSubmit}
+>
+  <Banner banner={bannerCfg} />
+  <p class="text-center">Delete the following permit?</p>
+  <table>
+    <tr>
+      <td class="font-bold">ID</td><td>{permit.id}</td>
+    </tr>
+    <tr>
+      <td class="font-bold">Resident ID</td><td>{permit.residentID}</td>
+    </tr>
+    <tr>
+      <td class="font-bold">License Plate</td><td>{permit.licensePlate}</td>
+    </tr>
+    <tr>
+      <td class="font-bold">Color</td><td>{permit.color}</td>
+    </tr>
+    <tr>
+      <td class="font-bold">Make</td><td>{permit.make}</td>
+    </tr>
+    <tr>
+      <td class="font-bold">Model</td><td>{permit.model}</td>
+    </tr>
+  </table>
+  <button type="submit" class="bg-rose-400 text-white text-center border rounded px-4 py-1">
+    Delete Permit
+  </button>
+</form>
