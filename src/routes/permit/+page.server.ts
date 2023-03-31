7e7d2fa7589fc988a87a4c5b20db6d697a8b675f
Daniel Velasquez <danivelas4@gmail.com>
2023-03-31 14:17:11 -0400

    src/routes/permit/+page.server.ts: cleaned up

--- src/routes/permit/+page.server.ts
+++ src/routes/permit/+page.server.ts
@@ -1,63 +1,27 @@
 import type { Actions } from "./$types";
-import { object, string, constant, optional, iso8601 } from "decoders";
-import { permitDecoder } from "$lib/models";
-import { validate } from "$lib/form";
-import { invalid, redirect } from "@sveltejs/kit";
-import { isOk } from "$lib/functional";
-import { post } from "$lib/api";
 import { getHeaderToken } from "$lib/server/auth";
-
-const formDecoder = object({
-  residentID: string,
-  licensePlate: string,
-  color: string,
-  make: string,
-  model: string,
-  startDate: iso8601,
-  endDate: iso8601,
-  isException: optional(constant("true")),
-  exceptionReason: optional(string),
-});
+import { isOk } from "$lib/functional";
+import { error } from "@sveltejs/kit";
+import { permitDecoder } from "$lib/models";
+import { Request } from "$lib/api";
 
 export const actions: Actions = {
   default: async (event) => {
     const formData = await event.request.formData();
     const formObject = Object.fromEntries(formData.entries());
-    const formRes = validate(formDecoder, formObject);
-    if (!isOk(formRes)) {
-      return invalid(400, { error: formRes.message });
-    }
-
-    // cast to form that backend requests
-    const permitReq = {
-      residentID: formRes.data.residentID,
-      car: {
-        licensePlate: formRes.data.licensePlate.toLocaleUpperCase(),
-        color: formRes.data.color,
-        make: formRes.data.make,
-        model: formRes.data.model,
-      },
-      startDate: formRes.data.startDate,
-      endDate: formRes.data.endDate,
-      exceptionReason: formRes.data.exceptionReason || "",
-    };
 
     const tokenRes = getHeaderToken(event.request.headers);
     if (!isOk(tokenRes)) {
-      return invalid(401, { response: tokenRes.message });
+      return error(401, { message: tokenRes.message });
     }
 
-    const result = await post("api/permit", permitReq, permitDecoder, tokenRes.data);
+    const result = await new Request(permitDecoder)
+      .setAccessToken(tokenRes.data)
+      .post("api/permit", formObject);
     if (!isOk(result)) {
-      if (result.message.includes("401")) {
-        return invalid(401, {
-          error:
-            "Your session has expired. Please log out and log back in again to create a permit.",
-        });
-      }
-      return invalid(400, { error: result.message });
+      return error(400, { message: result.message });
     }
 
-    throw redirect(302, `/permit/${result.data.id}`);
+    return { result };
   },
 };
