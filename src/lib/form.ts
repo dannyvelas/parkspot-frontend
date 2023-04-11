56076578c0f16cfe49b6ae8b9afa08437dd55b80
Daniel Velasquez <danivelas4@gmail.com>
2023-04-11 12:58:36 -0400

    src/lib/form.ts: un-deleted. still being used by login

---
+++ src/lib/form.ts
@@ -0,0 +1,63 @@
+import type { ActionResult } from "@sveltejs/kit";
+import type { Result } from "$lib/functional";
+import type { Decoder } from "decoders";
+import { newOk, newErr } from "$lib/functional";
+import { applyAction } from "$app/forms";
+import { invalidateAll } from "$app/navigation";
+import { tokenStore, getLatestToken } from "$lib/auth";
+import { get } from "svelte/store";
+
+export function validate<T>(
+  decoder: Decoder<T>,
+  obj: unknown,
+  emptyOk: boolean = false
+): Result<T> {
+  const decodeRes = decoder.decode(obj);
+  if (!decodeRes.ok) {
+    return newErr("Program error, please notify the administration to fix this");
+  }
+
+  if (emptyOk) {
+    return newOk(decodeRes.value);
+  }
+
+  const missing = Object.entries(decodeRes.value)
+    .filter(([_, v]) => v === "")
+    .map(([k, _]) => k);
+  if (missing.length > 0) {
+    return newErr(`Missing fields: ${missing.join(", ")}`);
+  }
+
+  return newOk(decodeRes.value);
+}
+
+type opts = {
+  formData?: FormData; // optionally override default formdata
+  resetForm?: boolean; // optionally reset form values after submit
+  allowRefresh?: boolean; // optionally explicitly prevent a token refresh
+};
+
+export async function submitWithToken(form: HTMLFormElement, opts?: opts) {
+  const justFormData = opts?.formData ?? new FormData(form);
+
+  const accessToken =
+    opts?.allowRefresh === undefined || opts?.allowRefresh === true
+      ? await getLatestToken()
+      : get(tokenStore);
+
+  const response = await fetch(form.action, {
+    method: "POST",
+    body: justFormData,
+    headers: new Headers({ Authorization: `Bearer ${accessToken}` }),
+  });
+
+  const result: ActionResult = await response.json();
+  if (result.type === "success") {
+    await invalidateAll();
+    if (opts?.resetForm === undefined || opts.resetForm === true) {
+      form.reset();
+    }
+  }
+
+  applyAction(result);
+}
