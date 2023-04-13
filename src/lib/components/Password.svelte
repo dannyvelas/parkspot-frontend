d2df13cf4e6c2e51729ff291fda092b2423e556e
Daniel Velasquez <danivelas4@gmail.com>
2023-04-13 09:46:12 -0400

    src/routes/residents/components/CreateResident.svelte: v1 src/lib/components/Password.svelte: added for use in component above

---
+++ src/lib/components/Password.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  // props
+  export let name: string;
+  export let placeholder: string;
+
+  // model
+  let passwordsShown = false;
+  $: passwordType = passwordsShown ? "text" : "password";
+  $: icon = passwordsShown ? "mdi:eye" : "mdi:eye-off";
+
+  // events
+  function toggleType() {
+    passwordsShown = !passwordsShown;
+  }
+</script>
+
+<div class="flex">
+  <input
+    required
+    type={passwordType}
+    class="grow border border-r-0 rounded-l p-2"
+    {name}
+    {placeholder}
+  />
+  <button type="button" class="border rounded-r flex items-center px-2" on:click={toggleType}>
+    <iconify-icon {icon} />
+  </button>
+</div>
---
+++ src/routes/residents/components/CreateResident.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { Request } from "$lib/api";
+  import { getLatestToken } from "$lib/auth";
+  import { isOk } from "$lib/functional";
+  import { residentDecoder } from "$lib/models";
+  import { createEventDispatcher } from "svelte";
+  import Banner, { updateBanner, clearBanner } from "$lib/components/Banner.svelte";
+  import Password from "$lib/components/Password.svelte";
+
+  // config
+  const dispatch = createEventDispatcher();
+
+  // events
+  async function handleSubmit() {
+    const formData = new FormData(this);
+    const formObject = Object.fromEntries(formData.entries());
+    const result = await new Request(residentDecoder)
+      .setAccessToken(await getLatestToken())
+      .post("api/resident", formObject);
+    if (!isOk(result)) {
+      updateBanner(true, result.message);
+      return;
+    }
+
+    clearBanner();
+    dispatch("created", result.data);
+  }
+</script>
+
+<form
+  class="bg-white flex flex-col mx-auto w-52 md:w-64 gap-4"
+  on:submit|preventDefault={handleSubmit}
+>
+  <Banner />
+  <p class="text-center font-bold text-lg">Create Resident</p>
+  <input required class="border rounded p-2" name="residentID" placeholder="Enter Resident ID" />
+  <input required class="border rounded p-2" name="firstName" placeholder="Enter First Name" />
+  <input required class="border rounded p-2" name="lastName" placeholder="Enter Last Name" />
+  <input required class="border rounded p-2" name="phone" placeholder="Enter Phone" />
+  <input required class="border rounded p-2" name="email" placeholder="Enter Email" />
+  <Password name="password" placeholder="Enter Password" />
+  <Password name="confirmPassword" placeholder="Confirm Password" />
+  <button type="submit" class="bg-green-400 text-white text-center border rounded px-4 py-1">
+    Create Resident
+  </button>
+</form>
