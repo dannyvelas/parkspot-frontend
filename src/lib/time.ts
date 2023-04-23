3427151a9f2eda5fb7015a6792b7082437671868
Daniel Velasquez <danivelas4@gmail.com>
2023-04-23 18:13:51 -0400

    src/lib/time.ts: changed dateToYmd function to `monthDayYearDate`

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,7 +1,5 @@
-export const dateToYmd = (date: Date): string => {
-  const offset = date.getTimezoneOffset();
-  const newDate = new Date(date.getTime() - offset * 60 * 1000);
-  return newDate.toISOString().split("T")[0];
+export const monthDayYearDate = (d: Date): string => {
+  return d.toLocaleDateString("en-us", { month: "short", day: "2-digit", year: "numeric" });
 };
 
 export const minimalDate = (d: Date): string => {
@@ -29,4 +27,8 @@ export const getEndOfTomorrow = () => {
   tempDate.setDate(tempDate.getDate() + 1);
   tempDate.setHours(23, 59, 59, 0);
   return tempDate;
+};
+
+export const getEndOfTime = () => {
+  return new Date(Date.UTC(9999, 11, 31, 23, 59, 59));
 };
