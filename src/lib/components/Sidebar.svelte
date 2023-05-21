114854d2ef0faa6283dfe2b41b2f0ebdb4c75431
Daniel Velasquez <danivelas4@gmail.com>
2023-05-21 17:52:09 -0400

    src/lib/components/Sidebar.svelte: added cars

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -15,6 +15,7 @@
     { name: "Dashboard", path: "/dashboard", icon: "material-symbols:dashboard-outline-rounded" },
     { name: "Permits", path: "/permits/all", icon: "clarity:details-line" },
     { name: "Residents", path: "/residents", icon: "uit:house-user" },
+    { name: "Cars", path: "/cars", icon: "la:car-side" },
     { name: "Visitors", path: "/visitors/active", icon: "material-symbols:badge-outline" },
   ];
 
