f06e76e5119def321838e14cb818fcac8dcb72c0
Daniel Velasquez <danivelas4@gmail.com>
2023-09-04 20:22:50 -0400

    src/routes/signup/: rough-draft of signup page

---
+++ src/routes/signup/+page.server.ts
@@ -0,0 +1,40 @@
+import type { Actions } from "./$types";
+import { sessionDecoder } from "$lib/auth";
+import { newRefresh } from "$lib/server/auth";
+import { fail } from "@sveltejs/kit";
+import { isOk } from "$lib/functional";
+import { Request } from "$lib/api";
+import { PUBLIC_ENV, PUBLIC_COOKIEDOMAIN } from "$env/static/public";
+
+export const actions: Actions = {
+  default: async (event) => {
+    const formData = await event.request.formData();
+    const formObject = Object.fromEntries(formData.entries());
+
+    const result = await new Request(sessionDecoder).post("api/login", formObject);
+    if (!isOk(result)) {
+      let error = "Unhandled error. Please notify the administration or try again later.";
+      if (result.message.includes("Unauthorized")) {
+        error = "Wrong username or password. Please try again.";
+      } else if (result.message.includes("Failed to fetch")) {
+        error = "Couldn't connect to server. Please notify the administration or try again later.";
+      } else if (result.message.includes("500")) {
+        error = "Server error. Please notify the administration or try again later.";
+      }
+      return fail(400, { error });
+    }
+
+    // TODO: get refresh token from server and forward it, instead of creating our own
+    const refreshToken = await newRefresh(result.data.user);
+    event.cookies.set("refresh", refreshToken, {
+      sameSite: "strict",
+      secure: PUBLIC_ENV == "prod",
+      domain: PUBLIC_COOKIEDOMAIN,
+    });
+
+    return { user: result.data.user, accessToken: result.data.accessToken };
+    // success causes sveltekit to run invalidateAll() which causes +layout.server.ts to re-run
+    // when it does, it will see the refresh cookie and initialize a session. the login page load
+    // function will see this session and redirect the user to their dashboard
+  },
+};
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  import Password from "$lib/components/Password.svelte";
+
+  // props
+  export let form: Record<string, any> | undefined;
+</script>
+
+<svelte:head>
+  <title>ParkSpot Sign Up</title>
+</svelte:head>
+{#if form?.error}
+  <div style="text-align: center">
+    <p>{form.error}</p>
+  </div>
+{/if}
+<form method="POST" class="flex flex-col mx-auto w-64 gap-4" use:enhance>
+  <input required class="border rounded p-2" name="communityName" placeholder="Community Name" />
+  <input required class="border rounded p-2" name="email" type="username" placeholder="Email" />
+  <Password name="password" placeholder="Password" />
+  <Password name="confirmPassword" placeholder="Confirm Password" />
+  <button type="submit" class="bg-green-400 text-white text-center border rounded px-4 py-1">
+    Sign Up
+  </button>
+</form>
