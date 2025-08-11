5237b90a396729fc4f14175fc0ab6cf31650e3f5
Daniel Velasquez <danivelas4@gmail.com>
2025-08-11 14:55:53 -0400

    small wording change M src/lib/i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -8,7 +8,7 @@ function initDictionary(seedLocale: string) {
       listName: "What's New?",
       l0: "You can now use a sidebar to navigate between pages. This sidebar has links where you can see your permits, cars, and visitors.",
       l1: "You can now see all of the cars that you've created visitor parking permits for.",
-      l2: "To create a parking permit, you can choose from the cars you've previously created permits for.",
+      l2: "In the permit page, you can choose from the cars you've previously created permits for.",
       l3: 'To add a new permit, car, or visitor, go the permit/car/visitor page. And, click on the "Create" button to the right of the search bar.',
     },
     es: {
@@ -17,9 +17,10 @@ function initDictionary(seedLocale: string) {
       listName: "¿Qué hay de nuevo?",
       l0: "Ahora podrás navegar nuestra aplicación con la nueva barra lateral. Ahí mismo encontrarás enlaces para ver tus permisos, carros y visitantes.",
       l1: "Creamos la página de carros, donde podrás ver la lista de todos los carros que han usado un permiso de visitante",
-      l2: 'Al crear un permiso nuevo, puedes escoger de tu lista de "carros visitantes"',
+      l2: 'En la página de permisos, puedes escoger de tu lista de "carros visitantes"',
       l3: 'Para crear un permiso, sencillamente ve a la página de permisos, y haz clic en el botón que dice "Create" en la parte derecha de la barra de búsqueda.',
     },
+
   });
   locale.set(seedLocale);
 }
